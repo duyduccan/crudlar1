75e51e838f8c6b66eb2e5757c563d02b607290d1
duc <daihocmodiachat1997@gmail.com>
2019-07-11 20:49:35 +0700

    init

--- resources/views/admin/layouts/layouts.blade.php
+++ resources/views/admin/layouts/layouts.blade.php
@@ -13,25 +13,25 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <link href="{{asset('web/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
+    <link href="{{secure_asset('web/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
     <!-- Custom Theme files -->
-    <link href="{{asset('web/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
+    <link href="{{secure_asset('web/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
     <!--js-->
-    <script src="{{asset('web/js/jquery-2.1.1.min.js')}}"></script>
+    <script src="{{secure_asset('web/js/jquery-2.1.1.min.js')}}"></script>
     <!--icons-css-->
-    <link href="{{asset('web/css/font-awesome.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('web/css/font-awesome.css')}}" rel="stylesheet">
     <!--Google Fonts-->
     <link href='//fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'>
     <!--static chart-->
-    <script src="{{asset('web/js/Chart.min.js')}}"></script>
+    <script src="{{secure_asset('web/js/Chart.min.js')}}"></script>
     <!--//charts-->
     <!-- geo chart -->
     <script src="//cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
     <script>window.modernizr || document.write('<script src="lib/modernizr/modernizr-custom.js"><\/script>')</script>
     <!--<script src="lib/html5shiv/html5shiv.js"></script>-->
     <!-- Chartinator  -->
-    <script src="{{asset('web/js/chartinator.js')}}" ></script>
+    <script src="{{secure_asset('web/js/chartinator.js')}}" ></script>
     <script type="text/javascript">
         jQuery(function ($) {
 
@@ -159,10 +159,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     });
 </script>
 <!--scrolling js-->
-<script src="{{asset('web/js/jquery.nicescroll.js')}}"></script>
-<script src="{{asset('web/js/scripts.js')}}"></script>
+<script src="{{secure_asset('web/js/jquery.nicescroll.js')}}"></script>
+<script src="{{secure_asset('web/js/scripts.js')}}"></script>
 <!--//scrolling js-->
-<script src="{{asset('web/js/bootstrap.js')}}"> </script>
+<script src="{{secure_asset('web/js/bootstrap.js')}}"> </script>
 <!-- mother grid end here-->
 </body>
 </html>
